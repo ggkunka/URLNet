05bc146415002968e695b3fc49ddfb7a40675288
Jayaprakash N S <60697280+ggkunka@users.noreply.github.com>
2024-10-25 12:30:30 +1100

    Update TextCNN.py

--- TextCNN.py
+++ TextCNN.py
@@ -1,157 +1,153 @@
-import tensorflow as tf 
+import tensorflow as tf
 
-class TextCNN(object): 
-    def __init__(self, char_ngram_vocab_size, word_ngram_vocab_size, char_vocab_size, \
-        word_seq_len, char_seq_len, embedding_size, l2_reg_lambda=0, \
-        filter_sizes=[3,4,5,6], mode=0): 
-        if mode == 4 or mode == 5: 
-            self.input_x_char = tf.placeholder(tf.int32, [None, None, None], name="input_x_char")
-            self.input_x_char_pad_idx = tf.placeholder(tf.float32, [None, None, None, embedding_size], name="input_x_char_pad_idx")
-        if mode == 4 or mode == 5 or mode == 2 or mode == 3: 
-            self.input_x_word = tf.placeholder(tf.int32, [None, None], name="input_x_word")
-        if mode == 1 or mode == 3 or mode == 5: 
-            self.input_x_char_seq = tf.placeholder(tf.int32, [None, None], name="input_x_char_seq") 
- 
-        self.input_y = tf.placeholder(tf.float32, [None, 2], name="input_y") 
-        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob") 
+class TextCNN(tf.keras.Model):
+    def __init__(self, char_ngram_vocab_size, word_ngram_vocab_size, char_vocab_size,
+                 word_seq_len, char_seq_len, embedding_size, l2_reg_lambda=0.0,
+                 filter_sizes=[3, 4, 5, 6], mode=0):
+        super(TextCNN, self).__init__()
+        self.mode = mode
+        self.l2_reg_lambda = l2_reg_lambda
+        self.word_seq_len = word_seq_len
+        self.char_seq_len = char_seq_len
+        self.embedding_size = embedding_size
+        self.filter_sizes = filter_sizes
+        self.num_filters = 256  # Number of filters per filter size
 
-        l2_loss = tf.constant(0.0) 
-        with tf.name_scope("embedding"): 
-            if mode == 4 or mode == 5: 
-                self.char_w = tf.Variable(tf.random_uniform([char_ngram_vocab_size, embedding_size], -1.0, 1.0), name="char_emb_w") 
-            if mode == 2 or mode == 3 or mode == 4 or mode == 5: 
-                self.word_w = tf.Variable(tf.random_uniform([word_ngram_vocab_size, embedding_size], -1.0, 1.0), name="word_emb_w")
-            if mode == 1 or mode == 3 or mode == 5: 
-                self.char_seq_w = tf.Variable(tf.random_uniform([char_vocab_size, embedding_size], -1.0, 1.0), name="char_seq_emb_w")        
-     
-            if mode == 4 or mode == 5: 
-                self.embedded_x_char = tf.nn.embedding_lookup(self.char_w, self.input_x_char)
-                self.embedded_x_char = tf.multiply(self.embedded_x_char, self.input_x_char_pad_idx)
-            if mode == 2 or mode == 3 or mode == 4 or mode == 5: 
-                self.embedded_x_word = tf.nn.embedding_lookup(self.word_w, self.input_x_word) 
-            if mode == 1 or mode == 3 or mode == 5: 
-                self.embedded_x_char_seq = tf.nn.embedding_lookup(self.char_seq_w, self.input_x_char_seq)  
+        # Regularization
+        self.l2_regularizer = tf.keras.regularizers.L2(l2_reg_lambda)
 
-            if mode == 4 or mode == 5: 
-                self.sum_ngram_x_char = tf.reduce_sum(self.embedded_x_char, 2)         
-                self.sum_ngram_x = tf.add(self.sum_ngram_x_char, self.embedded_x_word)
+        # Embedding layers
+        if mode in [4, 5]:
+            self.char_embedding = tf.keras.layers.Embedding(
+                input_dim=char_ngram_vocab_size,
+                output_dim=embedding_size,
+                embeddings_initializer='uniform',
+                name="char_embedding"
+            )
 
-            if mode == 4 or mode == 5: 
-                self.sum_ngram_x_expanded = tf.expand_dims(self.sum_ngram_x, -1) 
-            if mode == 2 or mode == 3: 
-                self.sum_ngram_x_expanded = tf.expand_dims(self.embedded_x_word, -1) 
-            if mode == 1 or mode == 3 or mode == 5: 
-                self.char_x_expanded = tf.expand_dims(self.embedded_x_char_seq, -1) 
-        
-        ########################### WORD CONVOLUTION LAYER ################################
-        if mode == 2 or mode == 3 or mode == 4 or mode == 5: 
-            pooled_x = []
+        if mode in [2, 3, 4, 5]:
+            self.word_embedding = tf.keras.layers.Embedding(
+                input_dim=word_ngram_vocab_size,
+                output_dim=embedding_size,
+                embeddings_initializer='uniform',
+                name="word_embedding"
+            )
 
-            for i, filter_size in enumerate(filter_sizes):
-                with tf.name_scope("conv_maxpool_%s" % filter_size): 
-                    filter_shape = [filter_size, embedding_size, 1, 256]
-                    b = tf.Variable(tf.constant(0.1, shape=[256]), name="b") 
-                    w = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="w") 
-                    conv = tf.nn.conv2d(
-                        self.sum_ngram_x_expanded,
-                        w,
-                        strides = [1,1,1,1],
-                        padding = "VALID",
-                        name="conv")
-                    h = tf.nn.relu(tf.nn.bias_add(conv,b), name="relu") 
-                    pooled = tf.nn.max_pool(
-                        h,
-                        ksize=[1, word_seq_len - filter_size + 1, 1, 1],
-                        strides=[1,1,1,1],
-                        padding="VALID",
-                        name="pool") 
-                    pooled_x.append(pooled) 
-        
-            num_filters_total = 256 * len(filter_sizes) 
-            self.h_pool = tf.concat(pooled_x, 3)
-            self.x_flat = tf.reshape(self.h_pool, [-1, num_filters_total], name="pooled_x")  
-            self.h_drop = tf.nn.dropout(self.x_flat, self.dropout_keep_prob, name="dropout_x") 
+        if mode in [1, 3, 5]:
+            self.char_seq_embedding = tf.keras.layers.Embedding(
+                input_dim=char_vocab_size,
+                output_dim=embedding_size,
+                embeddings_initializer='uniform',
+                name="char_seq_embedding"
+            )
 
-        ########################### CHAR CONVOLUTION LAYER ###########################
-        if mode == 1 or mode == 3 or mode == 5: 
-            pooled_char_x = []
-            for i, filter_size in enumerate(filter_sizes):
-                with tf.name_scope("char_conv_maxpool_%s" % filter_size):
-                    filter_shape = [filter_size, embedding_size, 1, 256]
-                    b = tf.Variable(tf.constant(0.1, shape=[256]), name="b")
-                    w = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="w")
-                    conv = tf.nn.conv2d(
-                        self.char_x_expanded,
-                        w,
-                        strides=[1,1,1,1],
-                        padding="VALID",
-                        name="conv")
-                    h = tf.nn.relu(tf.nn.bias_add(conv,b), name="relu")
-                    pooled = tf.nn.max_pool(
-                        h,
-                        ksize=[1, char_seq_len - filter_size + 1, 1, 1],
-                        strides=[1,1,1,1],
-                        padding="VALID",
-                        name="pool")
-                    pooled_char_x.append(pooled) 
-            num_filters_total = 256*len(filter_sizes) 
-            self.h_char_pool = tf.concat(pooled_char_x, 3)
-            self.char_x_flat = tf.reshape(self.h_char_pool, [-1, num_filters_total], name="pooled_char_x")
-            self.char_h_drop = tf.nn.dropout(self.char_x_flat, self.dropout_keep_prob, name="dropout_char_x")
-        
-        ############################### CONCAT WORD AND CHAR BRANCH ############################
-        if mode == 3 or mode == 5: 
-            with tf.name_scope("word_char_concat"): 
-                ww = tf.get_variable("ww", shape=(num_filters_total, 512), initializer=tf.contrib.layers.xavier_initializer())
-                bw = tf.Variable(tf.constant(0.1, shape=[512]), name="bw") 
-                l2_loss += tf.nn.l2_loss(ww) 
-                l2_loss += tf.nn.l2_loss(bw) 
-                word_output = tf.nn.xw_plus_b(self.h_drop, ww, bw)
+        # Convolution and pooling layers for word inputs
+        if mode in [2, 3, 4, 5]:
+            self.conv_layers = []
+            for filter_size in filter_sizes:
+                self.conv_layers.append(
+                    tf.keras.layers.Conv2D(
+                        filters=self.num_filters,
+                        kernel_size=(filter_size, embedding_size),
+                        activation='relu',
+                        kernel_initializer='glorot_uniform',
+                        name=f"conv_{filter_size}"
+                    )
+                )
 
-                wc = tf.get_variable("wc", shape=(num_filters_total, 512), initializer=tf.contrib.layers.xavier_initializer())
-                bc = tf.Variable(tf.constant(0.1, shape=[512]), name="bc") 
-                l2_loss += tf.nn.l2_loss(wc)
-                l2_loss += tf.nn.l2_loss(bc)
-                char_output = tf.nn.xw_plus_b(self.char_h_drop, wc, bc) 
-            
-                self.conv_output = tf.concat([word_output, char_output], 1)              
-        elif mode == 2 or mode == 4: 
-            self.conv_output = self.h_drop 
-        elif mode == 1: 
-            self.conv_output = self.char_h_drop        
+        # Convolution and pooling layers for character inputs
+        if mode in [1, 3, 5]:
+            self.char_conv_layers = []
+            for filter_size in filter_sizes:
+                self.char_conv_layers.append(
+                    tf.keras.layers.Conv2D(
+                        filters=self.num_filters,
+                        kernel_size=(filter_size, embedding_size),
+                        activation='relu',
+                        kernel_initializer='glorot_uniform',
+                        name=f"char_conv_{filter_size}"
+                    )
+                )
 
-        ################################ RELU AND FC ###################################
-        with tf.name_scope("output"): 
-            w0 = tf.get_variable("w0", shape=[1024, 512], initializer=tf.contrib.layers.xavier_initializer())
-            b0 = tf.Variable(tf.constant(0.1, shape=[512]), name="b0") 
-            l2_loss += tf.nn.l2_loss(w0) 
-            l2_loss += tf.nn.l2_loss(b0) 
-            output0 = tf.nn.relu(tf.matmul(self.conv_output, w0) + b0)
-            
-            w1 = tf.get_variable("w1", shape=[512, 256], initializer=tf.contrib.layers.xavier_initializer()) 
-            b1 = tf.Variable(tf.constant(0.1, shape=[256]), name="b1") 
-            l2_loss += tf.nn.l2_loss(w1) 
-            l2_loss += tf.nn.l2_loss(b1) 
-            output1 = tf.nn.relu(tf.matmul(output0, w1) + b1)
-            
-            w2 = tf.get_variable("w2", shape=[256,128], initializer=tf.contrib.layers.xavier_initializer())
-            b2 = tf.Variable(tf.constant(0.1, shape=[128]), name="b2") 
-            l2_loss += tf.nn.l2_loss(w2) 
-            l2_loss += tf.nn.l2_loss(b2) 
-            output2 = tf.nn.relu(tf.matmul(output1, w2) + b2) 
-            
-            w = tf.get_variable("w", shape=(128, 2), initializer=tf.contrib.layers.xavier_initializer()) 
-            b = tf.Variable(tf.constant(0.1, shape=[2]), name="b") 
-            l2_loss += tf.nn.l2_loss(w) 
-            l2_loss += tf.nn.l2_loss(b) 
-            
-            self.scores = tf.nn.xw_plus_b(output2, w, b, name="scores") 
-            self.predictions = tf.argmax(self.scores, 1, name="predictions") 
+        # Fully connected layers
+        total_filters = self.num_filters * len(filter_sizes)
+        if mode in [3, 5]:
+            fc_input_dim = total_filters * 2  # Concatenated word and char features
+        else:
+            fc_input_dim = total_filters
 
-        with tf.name_scope("loss"): 
-            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y) 
-            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+        self.dropout = tf.keras.layers.Dropout(rate=0.5)
+        self.fc1 = tf.keras.layers.Dense(512, activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.fc2 = tf.keras.layers.Dense(256, activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.fc3 = tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=self.l2_regularizer)
+        self.output_layer = tf.keras.layers.Dense(2, kernel_regularizer=self.l2_regularizer)
 
-        with tf.name_scope("accuracy"): 
-            correct_preds = tf.equal(self.predictions, tf.argmax(self.input_y, 1)) 
-            self.accuracy = tf.reduce_mean(tf.cast(correct_preds, "float"), name="accuracy") 
+    def call(self, inputs, training=False, dropout_keep_prob=1.0):
+        # Apply embeddings
+        pooled_outputs = []
+        l2_loss = 0.0
+
+        if self.mode in [4, 5]:
+            x_char = inputs['input_x_char']
+            x_char_pad_idx = inputs['input_x_char_pad_idx']
+            embedded_x_char = self.char_embedding(x_char)
+            embedded_x_char = tf.multiply(embedded_x_char, x_char_pad_idx)
+            sum_ngram_x_char = tf.reduce_sum(embedded_x_char, axis=2)
+
+        if self.mode in [2, 3, 4, 5]:
+            x_word = inputs['input_x_word']
+            embedded_x_word = self.word_embedding(x_word)
+
+        if self.mode in [1, 3, 5]:
+            x_char_seq = inputs['input_x_char_seq']
+            embedded_x_char_seq = self.char_seq_embedding(x_char_seq)
+
+        # Combine embeddings for word-level convolution
+        if self.mode in [4, 5]:
+            sum_ngram_x = sum_ngram_x_char + embedded_x_word
+            x_conv_input = tf.expand_dims(sum_ngram_x, -1)
+        elif self.mode in [2, 3]:
+            x_conv_input = tf.expand_dims(embedded_x_word, -1)
+
+        # Word-level convolution and pooling
+        if self.mode in [2, 3, 4, 5]:
+            pooled_outputs = []
+            for conv_layer in self.conv_layers:
+                conv = conv_layer(x_conv_input)
+                pool_size = (conv.shape[1], 1)
+                pooled = tf.keras.layers.MaxPool2D(pool_size=pool_size, strides=(1, 1), padding='valid')(conv)
+                pooled_outputs.append(pooled)
+
+            h_pool = tf.concat(pooled_outputs, axis=-1)
+            h_pool_flat = tf.reshape(h_pool, [-1, self.num_filters * len(self.filter_sizes)])
+            h_pool_flat = self.dropout(h_pool_flat, training=training and (dropout_keep_prob < 1.0))
+
+        # Character-level convolution and pooling
+        if self.mode in [1, 3, 5]:
+            char_x_conv_input = tf.expand_dims(embedded_x_char_seq, -1)
+            char_pooled_outputs = []
+            for conv_layer in self.char_conv_layers:
+                conv = conv_layer(char_x_conv_input)
+                pool_size = (conv.shape[1], 1)
+                pooled = tf.keras.layers.MaxPool2D(pool_size=pool_size, strides=(1, 1), padding='valid')(conv)
+                char_pooled_outputs.append(pooled)
+
+            h_char_pool = tf.concat(char_pooled_outputs, axis=-1)
+            h_char_pool_flat = tf.reshape(h_char_pool, [-1, self.num_filters * len(self.filter_sizes)])
+            h_char_pool_flat = self.dropout(h_char_pool_flat, training=training and (dropout_keep_prob < 1.0))
+
+        # Combine word and character features
+        if self.mode in [3, 5]:
+            conv_output = tf.concat([h_pool_flat, h_char_pool_flat], axis=1)
+        elif self.mode in [2, 4]:
+            conv_output = h_pool_flat
+        elif self.mode == 1:
+            conv_output = h_char_pool_flat
+
+        # Fully connected layers
+        output = self.fc1(conv_output)
+        output = self.fc2(output)
+        output = self.fc3(output)
+        logits = self.output_layer(output)
+
+        return logits

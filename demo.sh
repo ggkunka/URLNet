e726ee094ea34bbc4200d0ffc1a23fce2a1d5ec2
Jayaprakash N S <60697280+ggkunka@users.noreply.github.com>
2024-10-25 14:19:29 +1100

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-# Train the model
-python train.py \
-    --data_data_dir data/train_converted.txt \
-    --data_dev_pct 0.1 \
-    --data_delimit_mode 0 \
-    --data_min_word_freq 1 \
-    --model_emb_mode 1 \
-    --model_emb_dim 32 \
-    --model_filter_sizes 3,4,5,6 \
-    --train_nb_epochs 5 \
-    --train_batch_size 32 \
-    --log_output_dir runs/1000_emb1_dlm0_run/
+emb_modes=(1 2 2 3 3 4 5)
+delimit_modes=(0 0 1 0 1 1 1)
+train_size=100000
+test_size=100000
+nb_epoch=5
 
-# Test the model
-python test.py \
-    --data_data_dir data/train_converted.txt \
-    --data_delimit_mode 0 \
-    --data_word_dict_dir runs/1000_emb1_dlm0_run/words_dict.p \
-    --data_subword_dict_dir runs/1000_emb1_dlm0_run/subwords_dict.p \
-    --data_char_dict_dir runs/1000_emb1_dlm0_run/chars_dict.p \
-    --log_checkpoint_dir runs/1000_emb1_dlm0_run/checkpoints/ \
-    --log_output_dir runs/1000_emb1_dlm0_run/ \
-    --model_emb_mode 1 \
-    --model_emb_dim 32 \
-    --test_batch_size 32
+for ((i=0; i < ${#emb_modes[@]}; ++i)); do
+    python train.py \
+        --data_data_dir data/train_converted.txt \
+        --data_dev_pct 0.001 \
+        --data_delimit_mode ${delimit_modes[$i]} \
+        --data_min_word_freq 1 \
+        --model_emb_mode ${emb_modes[$i]} \
+        --model_emb_dim 32 \
+        --model_filter_sizes 3,4,5,6 \
+        --train_nb_epochs ${nb_epoch} \
+        --train_batch_size 1048 \
+        --log_print_every 5 \
+        --log_eval_every 10 \
+        --log_checkpoint_every 10 \
+        --log_output_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep/
 
+    python test.py \
+        --data_data_dir data/train_converted.txt \
+        --data_delimit_mode ${delimit_modes[$i]} \
+        --data_word_dict_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep/words_dict.p \
+        --data_subword_dict_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep/subwords_dict.p \
+        --data_char_dict_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep/chars_dict.p \
+        --log_checkpoint_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep/checkpoints/ \
+        --log_output_dir runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep/ \
+        --model_emb_mode ${emb_modes[$i]} \
+        --model_emb_dim 32 \
+        --test_batch_size 1048
 
-python auc.py --input_path runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep/ --input_file test_results.txt --threshold 0.5
+    python auc.py \
+        --input_path runs/${train_size}_emb${emb_modes[$i]}_dlm${delimit_modes[$i]}_32dim_minwf1_1conv3456_${nb_epoch}ep/ \
+        --input_file test_results.txt \
+        --threshold 0.5
 done
